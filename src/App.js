479315b37c50d1646373e0db87a169018654cc9e
Thaís Cristine Coelho <thaisholz@hotmail.com>
2025-03-05 16:18:06 +0000

    Criação do campo de texto e formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario/Formulario.js';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Formulario />
     </div>
   );
 }
